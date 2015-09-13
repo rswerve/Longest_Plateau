acca47f14a333099fbf403f58fe8b0cb0bf82f47
rswerve <rswerve@users.noreply.github.com>
2015-09-13 12:05:15 -0500

    Update longestPlateau.java

--- longestPlateau.java
+++ longestPlateau.java
@@ -1,4 +1,8 @@
-// Longest Plateau.  A plateau is a level stretch _above_ the surrounding environment.  In an array, find the longest run of the same number that is greater than the numbers immediately preceding and following it.  For example, in the sequence 3-2-2-2-1-3-4-4-1, 2-2-2 is *not* a plateau, because 2 is less than 3.  4-4 is the longest plateau in that sequence, because 4 is greater than 3 and greater than 1.
+/* Longest Plateau.  A plateau is a level stretch _above_ the surrounding environment.  
+In an array, find the longest run of the same number that is greater than the numbers 
+immediately preceding and following it.  For example, 
+in the sequence 3-2-2-2-1-3-4-4-1, 2-2-2 is *not* a plateau, because 2 is less than 3.  
+4-4 is the longest plateau in that sequence, because 4 is greater than 3 and greater than 1.*/
 
 
 public class longestPlateau
@@ -20,7 +24,7 @@ public class longestPlateau
 		}
 
 		int i = 1;  // begin at 1 because you'll be looking back to i-1, so
-					// beginning at 0 will give an error
+			    // beginning at 0 will give an error
 		while (i < (N-2))  //  you'll be looking ahead to i+2, so stop at N-2
 		{
 		// note the scope in what follows.  while i is less than N-2, check for a run.  if there's a run, count its
@@ -30,7 +34,7 @@ public class longestPlateau
 			if (a[i] == a[i+1])
 			{
 				++counter;  // if you have a run, increment the counter. the counter counts the 
-							// length of a run
+					    // length of a run
 				 
 				 // two major tests are here: is this run longer than other runs have been, and
 				 // is this really a plateau?  each longest run will overwrite highCount, but only if
@@ -43,8 +47,8 @@ public class longestPlateau
 				 {
 				 	highCount = counter;
 				 	bestStart = i-(counter-1);  // find the beginning of the run.  note that counter
-				 								// starts at zero, so a run of two 1-2-2-1 will set
-				 								// the counter to 1.
+				 				    // starts at zero, so a run of two 1-2-2-1 will set
+				 				   // the counter to 1.
 				 }
 			}
 			
@@ -57,10 +61,11 @@ public class longestPlateau
 			
 		}
 			
-		// print the array (if you want to check it yourself) and note the longest plateau.
-		// because of zero-based indexing, add 1 to highCount to match the beginning of 
-		// the run to the index.
-		// also note that this program finds the first longest run in the array.
+		/* print the array (if you want to check it yourself) and note the longest plateau.
+		because of zero-based indexing, add 1 to highCount to match the beginning of 
+		the run to the index.
+		also note that this program finds the first longest run in the array. */
+		
 		for (i = 0; i < N; ++i)
 		{
 		 	System.out.println(i + "\t" + a[i]);
